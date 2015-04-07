7f886f1db75125480c142189712ed28d8761ff77
Linda Parsons <lparsonstech@gmail.com>
2015-04-07 12:08:36 -0400

    Bump version: 2.0.9 → 2.0.10

--- source/conf.py
+++ source/conf.py
@@ -60,7 +60,7 @@ copyright = u'2014, 2015 David Dittrich'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0.9'
+version = '2.0.10'
 # The full version, including alpha/beta/rc tags.
 release = version
 
